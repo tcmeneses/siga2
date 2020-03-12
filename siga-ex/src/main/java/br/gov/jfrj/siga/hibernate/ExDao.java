28c7339a82d0a4dbc5aab8c3f9723bb8453103ee
jlo <joaoluis@trf2.jus.br>
2020-03-12 18:36:53 -0300

    DOC: Fix query da que busca documentos da Mesa para usar query do GOVSP.

--- siga-ex/src/main/java/br/gov/jfrj/siga/hibernate/ExDao.java
+++ siga-ex/src/main/java/br/gov/jfrj/siga/hibernate/ExDao.java
@@ -1512,8 +1512,8 @@ public class ExDao extends CpDao {
 				+ " inner join marca.exMobil mobil"
 				+ " where (marca.dtIniMarca is null or marca.dtIniMarca < sysdate)"
 				+ " and (marca.dtFimMarca is null or marca.dtFimMarca > sysdate)"
-				+ (titular != null ? " and (marca.dpPessoaIni = :titular)"
-						: " and (marca.dpLotacaoIni = :lotaTitular)");
+				+ (titular != null ? " and (marca.dpPessoaIni.idPessoaIni = :titular)"
+						: " and (marca.dpLotacaoIni.idLotacaoIni = :lotaTitular)");
 		if(SigaBaseProperties.getString("siga.local") != null && "GOVSP".equals(SigaBaseProperties.getString("siga.local"))) {
 			q += " and ((mobil.exDocumento.numExpediente is null and marcador = 1) or (mobil.exDocumento.numExpediente is not null))"
 					+ " and marcador <> 10";
@@ -1521,13 +1521,7 @@ public class ExDao extends CpDao {
 		long tempoIni = System.nanoTime();
 		Query query = em()
 				.createQuery(
-						"select marca, marcador, mobil from ExMarca marca"
-								+ " inner join marca.cpMarcador marcador"
-								+ " inner join marca.exMobil mobil"
-								+ " where (marca.dtIniMarca is null or marca.dtIniMarca < sysdate)"
-								+ " and (marca.dtFimMarca is null or marca.dtFimMarca > sysdate)"
-								+ (titular != null ? " and (marca.dpPessoaIni.idPessoaIni = :titular)"
-										: " and (marca.dpLotacaoIni.idLotacaoIni = :lotaTitular)"));
+						q);
 
 		if (titular != null)
 			query.setParameter("titular", titular.getIdPessoaIni());
